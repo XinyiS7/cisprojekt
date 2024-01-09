37fbba4ee5953698be672244f4b7d20708091612
AliciaT <aliciaju@126.com>
2024-01-09 20:00:12 +0100

    flag check

--- src/js/interact.js
+++ src/js/interact.js
@@ -10,10 +10,19 @@ function showprepare() {
   prepareObj.style.display = "block";
 }
 
-function hideresult() {
+function hideresult(){
   let resultObj = document.getElementById("result");
-  resultObj.style.display = "none";
-}
+  let switchbutton = document.getElementById("switchbtn");
+  resultObj.style.display='none';
+  if(switchbutton.innerHTML == "Full Screen"){
+      mapWindows.style.transform = "scale(1)";
+      mapWindows.style.left = "0px";
+      mapWindows.style.top = "10px";
+      clusterBox.style.display = "none";
+      switchbutton.innerHTML = "Exit Full Screen";
+  }
+  return 0;
+};
 
 function showresult() {
   let resultObj = document.getElementById("result");
@@ -57,6 +66,67 @@ function getfunctionflag() {
   return funcFlag;
 }
 
+/*When text-area not empty, will read the inhalt and check if the first line is the title line */
+function getTitleLine(InputFlag = "coord"){
+  console.log("trying to get the title Title line from", InputFlag, "data");
+  let inhalt = document.getElementById("text_box").value;
+  let lines = inhalt.split('\n');
+  let firstline = lines[0].split(',');
+  let titleline = new Array();
+  console.log("first line is:", firstline);
+  let dimension = firstline.length;
+  let havetitle = new Boolean(false);
+  //check if the first line is title line
+  switch (InputFlag){
+      case 'coord':
+          for(let m = 0; m < dimension; m++){
+              if(parseFloat(firstline[m])){
+                  continue;
+              }
+              else{
+                  
+                  havetitle = true;
+                  break;
+              }
+          }
+          break;
+      case 'molecur':
+          break;
+      default:
+          havetitle = false;
+          break;         
+  }
+
+  //if no titleline detected, then use the default title
+  if(havetitle == true){
+      console.log("title line detected");
+      titleline = firstline;
+  }
+  else{
+      console.log("using default title");
+      titleline = Array.from({length: dimension}, (v, x) => x+1);
+  }
+  console.log(titleline);
+  return titleline;
+}
+
+/*Creat the drop-down Menu accroding to the title line */
+function CreateColFlagSelector(titleline){
+  console.log("creating the ColFlag selector accroding to the titleline", titleline);
+  let ColFlagMenu = document.createElement("select");
+  let firstOption = document.createElement("option");
+  let dimension = titleline.length;
+  firstOption.value = "noColFlag";
+  firstOption.text = "select a colum flag";
+  for(let i = 0; i < dimension; i++){
+      let flagOption = document.createElement("option");
+      flagOption.value = titleline[i];
+      flagOption.text = titleline[i];
+      ColFlagMenu.appendChild(flagOption);
+  }
+  return ColFlagMenu;
+}
+
 /** This will check the first line and delete the axises */
 function isCoordindat(txt_inhalt) {
   let coorindat = [];
@@ -82,6 +152,45 @@ function isCoordindat(txt_inhalt) {
 function isSequence(txt_inhalt, matchflag) {}
 
 function isInChI(txt_inhalt, matchflag) {}
+
+//choos flags for each colum
+function ColFlagCheck(){
+  console.log("Now your running the test function.");
+  let titleline = getTitleLine(InputFlag = "coord");
+  let preset_flag = document.createElement("select");
+  let first_flag = document.createElement("option");
+  let checkContainer = document.getElementById("CheckContainer");
+  let checklist = document.getElementById("checkTable");
+  let dimension = titleline.length;
+  let rows = checklist.getElementsByTagName('tr');
+  while(rows.length > 1){
+      checklist.deleteRow(-1);
+  }
+
+  first_flag.value = "noChoice";
+  first_flag.text = "choose a flag";
+  let flag_list = new Array(["name", "distance information", "numerical additional information", "non-numericial additional information"])
+  flag_list.forEach(flag){
+    let _option = document.createElement("option");
+    _option.value = flag;
+    _option.text = flag;
+    preset_flag.appendChild(_option);
+  }
+
+  for(let d = 1; d < dimension+1; d++){
+      console.log(d+" Cloumn");
+      let _check_tr = checklist.insertRow();
+      let cell_1 = _check_tr.insertCell(0);
+      let cell_2 = _check_tr.insertCell(1);
+      let cell_3 = _check_tr.insertCell(2);
+      let ColFlagSelector = CreateColFlagSelector(titleline);
+
+      cell_1.textContent = 'the '+(d)+' Colum is:';
+      cell_2.appendChild(ColFlagSelector);
+      cell_3.appendChild(preset_flag);
+  }
+  checkContainer.appendChild(checklist);
+}
 
 /* Fullscreen in-Place */
 
@@ -470,3 +579,5 @@ function addFlag(flagType) {
   // Call the function to populate the dropdown
   showDropdowns();
 }
+
+
